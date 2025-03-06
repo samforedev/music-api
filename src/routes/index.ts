0e491a9e8d40869fd47209382a14a2a0ecb097b9
Samuel Forero <samuelf.dev@outlook.com>
2025-03-06 00:57:57 -0500

    Add: Band index routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import artistRoutes from "./artist.routes";
+import bandRoutes from "./band.routes";
 
 const router = Router();
 
 /** Artist Router */
 router.use("/artists", artistRoutes);
+
+/** Bands Router */
+router.use("/bands", bandRoutes);
 
 export default router;
