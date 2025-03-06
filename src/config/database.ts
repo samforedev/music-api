cb2eb309f5a69f3ca4ba2c81d4a8d220652b4bd6
Samuel Forero <samuelf.dev@outlook.com>
2025-03-06 17:22:28 -0500

    Update: Database connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 import { MONGO_URI } from "../models/constants";
 
-const connectDB = async(): Promise<void> => {
+const connectDB = async (): Promise<void> => {
     try {
         const database = await mongoose.connect(MONGO_URI);
-        console.log(`✅ Connection to Database: ${database.connection.name} Successfully`);
+        console.log(`✅ Connection to Database (ATLAS): ${database.connection.name} Successfully`);
     } catch (err) {
         console.error("❌ Connection to Database Error", err);
         process.exit(1);
